6e9130c5e9f21b0a6f05146924bd27ff20cb5d8e
Shreya Bista <bistashreya@gmail.com>
2024-04-09 15:19:49 +1000

    commit4

--- src/main/java/sit707_tasks/Main.java
+++ src/main/java/sit707_tasks/Main.java
@@ -34,108 +34,125 @@ public class Main
         /*
          * January min boundary area: min+1, min-1
          */
-        System.out.println("January min increment should be 2nd January.");
+        System.out.println("January min increment should be 2 January");
     	date = new DateUtil(1, 1, 2024);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("January min decrement should be 31 December previous year.");
+        System.out.println("January min decrement should be 31 December previous year");
         date = new DateUtil(1, 1, 2024);
         System.out.println(date);
         date.decrement();
         System.out.println(date); 
         
-        System.out.println("February max Leap increment should be 1st March.");
+        System.out.println("February Leap year increment should be March 1");
     	date = new DateUtil(29, 2, 2024);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("February min Leap decrement should be 28 Feb.");
+        System.out.println("February Leap year decrement should be February 28");
         date = new DateUtil(29, 2, 2024);
         System.out.println(date);
         date.decrement();
+        System.out.println(date);  
+
+        System.out.println("March 1 Lear year decrement should be February 29");
+        date = new DateUtil(1, 3, 2024);
+        System.out.println(date);
+        date.decrement();
         System.out.println(date);   
+	}
+        System.out.println("Not leap year March 1 decrement should be February 28");
+        date = new DateUtil(1, 3, 2023);
+        System.out.println(date);
+        date.decrement();
+        System.out.println(date);  
         
-        System.out.println("June increment should be 2nd June.");
+        System.out.println("June increment should be June 2");
     	date = new DateUtil(1, 6, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 3rd June.");
+        System.out.println("June increment should be June 3");
     	date = new DateUtil(2, 6, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 16th June.");
+        System.out.println("June increment should be June 16");
     	date = new DateUtil(15, 6, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 1st July.");
+        System.out.println("June increment should be July 1");
     	date = new DateUtil(30, 6, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June 31 increment should be 1st July.");
+        System.out.println("June increment should be July 1");
     	date = new DateUtil(31, 6, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
      
-        System.out.println("Jan increment should be 16th Jan.");
+        System.out.println("Jan increment should be Jan 16");
     	date = new DateUtil(15, 1, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("Feb increment should be 16th Feb.");
+        System.out.println("Feb increment should be Feb 16");
     	date = new DateUtil(15, 2, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("Nov increment should be 16th Nov.");
+        System.out.println("Nov increment should be Nov 16");
     	date = new DateUtil(15, 11, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("Dec increment should be 16th Dec.");
+        System.out.println("Dec increment should be Dec 16");
     	date = new DateUtil(15, 12, 1994);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 16th June,1700.");
+        System.out.println("June increment should be June 16,1700");
     	date = new DateUtil(15, 6, 1700);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 16th June,1701.");
+        System.out.println("June increment should be June 16 1701");
      	date = new DateUtil(15, 6, 1701);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 16th June,2023.");
+        System.out.println("June increment should be June 16 2023");
      	date = new DateUtil(15, 6, 2023);
         System.out.println(date);
         date.increment();
         System.out.println(date);
         
-        System.out.println("June increment should be 16th June,2024.");
+        System.out.println("June increment should be June 16 2024");
      	date = new DateUtil(15, 6, 2024);
         System.out.println(date);
         date.increment();
         System.out.println(date);
-
+	}
+	    System.out.println("january decrement should be december 31 2023");
+     	date = new DateUtil(1, 1, 2024);
+        System.out.println(date);
+        date.increment();
+        System.out.println(date);
      
     }
 }
--- src/test/java/sit707_tasks/DateUtilTest.java
+++ src/test/java/sit707_tasks/DateUtilTest.java
@@ -22,7 +22,7 @@ public class DateUtilTest {
 	public void testMaxJanuary31ShouldIncrementToFebruary1() {
 		// January max boundary area: max+1
 		DateUtil date = new DateUtil(31, 1, 2024);
-        System.out.println("january31ShouldIncrementToFebruary1 > " + date);
+        System.out.println("January 31 should increment to february 1 > " + date);
         date.increment();
         System.out.println(date);
         Assert.assertEquals(2, date.getMonth());
@@ -33,7 +33,7 @@ public class DateUtilTest {
 	public void testMaxJanuary31ShouldDecrementToJanuary30() {
 		// January max boundary area: max-1
 		DateUtil date = new DateUtil(31, 1, 2024);
-        System.out.println("january31ShouldDecrementToJanuary30 > " + date);
+        System.out.println("January 31 should decrement to january 30 > " + date);
         date.decrement();
         System.out.println(date);
         Assert.assertEquals(30, date.getDay());
@@ -44,7 +44,7 @@ public class DateUtilTest {
 	public void testNominalJanuary() {
 		int rand_day_1_to_31 = 1 + new Random().nextInt(31);
         DateUtil date = new DateUtil(rand_day_1_to_31, 1, 2024);
-        System.out.println("testJanuaryNominal > " + date);
+        System.out.println("testing January Nominal > " + date);
         date.increment();
         System.out.println(date);
 	}
@@ -52,7 +52,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJanuary1ShouldIncrementToJanuary2() {
 	    DateUtil date = new DateUtil(1, 1, 2024);
-	    System.out.println("MinJanuary1ShouldIncrementToJanuary2 > " + date);
+	    System.out.println("January 1 Should Increment To January2 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(2, date.getDay());
@@ -62,7 +62,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJanuary1ShouldDecrementToDecember31() {
 	    DateUtil date = new DateUtil(1, 1, 2024);
-	    System.out.println("MinJanuary1ShouldDecrementToDecember31 > " + date);
+	    System.out.println("Min January 1Should Decrement To December31 > " + date);
 	    date.decrement();
 	    System.out.println(date);
 	    Assert.assertEquals(31, date.getDay());
@@ -77,7 +77,7 @@ public class DateUtilTest {
 	@Test
 	public void testLeapYearFebruary29_ShouldIncrementToMarch1() {
 		DateUtil date = new DateUtil(29, 2, 2024);
-		System.out.println("LeapYearFebruary29_ShouldIncrementToMarch1 > " + date);
+		System.out.println("Leap year feb 29 should increment to march 1 > " + date);
 		date.increment();
 		System.out.println(date);
 		Assert.assertEquals(3, date.getMonth());
@@ -87,17 +87,37 @@ public class DateUtilTest {
 	@Test
 	public void testLeapYearFebruary29_ShouldDecrementToFebruary28() {
 		DateUtil date = new DateUtil(29, 2, 2024);
-		System.out.println("LeapYearFebruary29_ShouldDecrementToFebruary28 > " + date);
+		System.out.println("Leap year February29 should decrement To February 28 > " + date);
 		date.decrement();
 		System.out.println(date);
 		Assert.assertEquals(28, date.getDay());
 		Assert.assertEquals(2, date.getMonth());
 	}
+	@Test
+	public void testLeapYearMarch1_ShouldDecrementToFebruary29() {
+		DateUtil date = new DateUtil(1, 3, 2024);
+		System.out.println("Leap Year March 1 Should Decrement To February 29 > " + date);
+		date.decrement();
+		System.out.println(date);
+		Assert.assertEquals(29, date.getDay());
+		Assert.assertEquals(2, date.getMonth());
+	    Assert.assertEquals(2024, date.getYear());
+	}
+	@Test
+	public void testNotLeapYearMarch1_ShouldDecrementToFebruary28() {
+		DateUtil date = new DateUtil(1, 3, 2023);
+		System.out.println("Not Leap Year March 1 should decrement To February 28 > " + date);
+		date.decrement();
+		System.out.println(date);
+		Assert.assertEquals(28, date.getDay());
+		Assert.assertEquals(2, date.getMonth());
+	    Assert.assertEquals(2023, date.getYear());
+	}
 	
 	@Test
 	public void testMinJune1_ShouldIncrementToJune2() {
 	    DateUtil date = new DateUtil(1, 6, 1994);
-	    System.out.println("MinJune1_ShouldIncrementToJune2 > " + date);
+	    System.out.println("June 1 should increment to june 2 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(2, date.getDay());
@@ -106,7 +126,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune2_ShouldIncrementToJune3() {
 	    DateUtil date = new DateUtil(2, 6, 1994);
-	    System.out.println("MinJune2_ShouldIncrementToJune3 > " + date);
+	    System.out.println("June 2 should increment to june 3 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(3, date.getDay());
@@ -116,7 +136,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune15_ShouldIncrementToJune16() {
 	    DateUtil date = new DateUtil(15, 6, 1994);
-	    System.out.println("MinJune15_ShouldIncrementToJune16 > " + date);
+	    System.out.println("June 15 should increment to june 16 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -126,7 +146,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune30_ShouldIncrementToJuly1() {
 	    DateUtil date = new DateUtil(30, 6, 1994);
-	    System.out.println("MinJune30_ShouldIncrementToJuly1 > " + date);
+	    System.out.println("June 30 should increment to July 1 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(1, date.getDay());
@@ -146,7 +166,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJan15_ShouldIncrementToJan16() {
 	    DateUtil date = new DateUtil(15, 1, 1994);
-	    System.out.println("MinJan15_ShouldIncrementToJan16 > " + date);
+	    System.out.println("Jan 15 should increment to jan 16 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -156,7 +176,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinFeb15_ShouldIncrementToFeb16() {
 	    DateUtil date = new DateUtil(15, 2, 1994);
-	    System.out.println("MinFeb15_ShouldIncrementToFeb16 > " + date);
+	    System.out.println("Feb 15 should increment to Feb 16 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -166,7 +186,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinNov15_ShouldIncrementToNov16() {
 	    DateUtil date = new DateUtil(15, 11, 1994);
-	    System.out.println("MinNov15_ShouldIncrementToNov16 > " + date);
+	    System.out.println("Nov 15 should increment to nov 16 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -176,7 +196,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinDec15_ShouldIncrementToDec16() {
 	    DateUtil date = new DateUtil(15, 12, 1994);
-	    System.out.println("MinDec15_ShouldIncrementToDec16 > " + date);
+	    System.out.println("Dec 15 should increment to Dec 16 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -186,7 +206,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune15_ShouldIncrementToJune16_1700() {
 	    DateUtil date = new DateUtil(15, 6, 1700);
-	    System.out.println("MinJune15_ShouldIncrementToJune16_1700 > " + date);
+	    System.out.println("June 15 should increment to june16_1700 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -196,7 +216,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune15_ShouldIncrementToJune16_1701() {
 	    DateUtil date = new DateUtil(15, 6, 1701);
-	    System.out.println("MinJune15_ShouldIncrementToJune16_1701 > " + date);
+	    System.out.println("June 15 should increment to June16_1701 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -206,7 +226,7 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune15_ShouldIncrementToJune16_2023() {
 	    DateUtil date = new DateUtil(15, 6, 2023);
-	    System.out.println("MinJune15_ShouldIncrementToJune16_2023 > " + date);
+	    System.out.println("June 15 should increment to june16_2023 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
@@ -216,13 +236,23 @@ public class DateUtilTest {
 	@Test
 	public void testMinJune15_ShouldIncrementToJune16_2024() {
 	    DateUtil date = new DateUtil(15, 6, 2024);
-	    System.out.println("MinJune15_ShouldIncrementToJune16_2024 > " + date);
+	    System.out.println("June 15 should increment To june16_2024 > " + date);
 	    date.increment();
 	    System.out.println(date);
 	    Assert.assertEquals(16, date.getDay());
 	    Assert.assertEquals(6, date.getMonth());
 	}
-	
+
+	@Test
+	public void testMaxJanuary1_2024_ShouldDecrementToDec31_2023() {
+		DateUtil date = new DateUtil(1, 1, 2024);
+        System.out.println("january 1 2024 should decrement to december 31 2023 > " + date);
+        date.decrement();
+        System.out.println(date);
+        Assert.assertEquals(31, date.getDay());
+        Assert.assertEquals(12, date.getMonth());
+		Assert.assertEquals(2023, date.getYear());
+	}
 	
 
 }
